6c3d4ebf9db4c7c63d7b88bb81434ba96d5fb646
bakdoolot <altybaevbaku15@gmail.com>
2023-06-23 00:19:41 +0600

    Initial commit
    add container, header

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Box } from '@mui/material'
+import Header from './components/header/Header'
+
+function App() {
+
+  return (
+    <Box maxWidth="mdlarge" sx={{ m: "0 auto" }}>
+      <Header/>
+      asdfsd
+    </Box>
+  )
+}
+
+export default App
